76a1f6d3a942edd69ab27201200d7c722366fa65
Douglas Oliveira <douglinhas_uchiha@hotmail.com>
2016-12-01 15:49:26 -0200

    Failed

--- routes.js
+++ routes.js
@@ -1,7 +1,14 @@
+
 const path = __dirname + "/public/"
 
 module.exports = function(app) {
     app.use("/", function(request, response) {
         response.sendFile(path + "index.html");
     })
+
+    app.post("/calcular", function(request, response){
+        console.log("Salary: " + request.body);  //ver no terminal
+        response.send("Salary: " + request.body); //pegar o valor
+    })
 }
+
